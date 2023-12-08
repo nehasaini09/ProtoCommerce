fc8618f49de99096fd1c6c8dc9836eb028a0e76d
Neha <nehasaini.mca09@gmail.com>
2023-12-08 13:12:07 -0700

    commit

--- src/test/java/controllerInterface/ControllerInterface.java
+++ src/test/java/controllerInterface/ControllerInterface.java
@@ -1,5 +1,12 @@
 package controllerInterface;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public interface ControllerInterface {
+	
+	//abstract methods for user actions
+	public void click(WebDriver driver,WebElement element);
+	public String getTitle(WebDriver driver);
 
 }
