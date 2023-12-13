b8a4d5a5830bc40434a69b58a1da946e49f4423f
sreej <sreej@deepika-evo>
2023-12-13 12:22:29 -0500

    commiting

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -5,7 +5,7 @@ import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features="src/test/resources/features/home2/homePageLaunch.feature",
+@CucumberOptions(features="src/test/resources/features/", //home2/homePageLaunch.feature
 glue="stepDefinitions",monochrome=true,plugin= {"html:target/cucumber.html",
 		"json:target/cucumber.json"})
 		
